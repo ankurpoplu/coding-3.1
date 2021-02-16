c33567ad50100dc4080346e21513892080d6f3a2
ankur <ankurp31031@gmail.com>
2021-02-16 06:12:58 +0530

    add

--- class3 D12.2/reactboom/src/grocery/Grocery.jsx
+++ class3 D12.2/reactboom/src/grocery/Grocery.jsx
@@ -4,14 +4,14 @@ import style from "./grocery.module.css"
 import Output from "./Output"
 
 const Grocery = ()=>{
-    const increaseCount = (e)=>{}
+    
     return(
         <div className={style.Grocery}>
             <h1 id={style.tit}>Grocery List</h1>
             <div id={style.pag}>
-                
+
             </div>
-            {data.map(e=><Output className={style.box} key={data.id} data={e} increaseCount={increaseCount}></Output>)}
+            {data.map(e=><Output className={style.box} key={data.id} data={e} ></Output>)}
         </div>
     )
 }
--- class3 D12.2/reactboom/src/grocery/Output.jsx
+++ class3 D12.2/reactboom/src/grocery/Output.jsx
@@ -2,9 +2,9 @@ import React,{useState} from "react"
 import style from "./output.module.css"
 import { AiOutlineMinus,AiOutlinePlus } from "react-icons/ai";
 
-const Output = ({data,increaseCount,decreaseCount})=>{
-    const {product_name,image_url,description,is_available,price} = data
-    let count = 0
+const Output = ({data})=>{
+    const [counter,setCounteer] = useState(0)
+    const {product_name,image_url,description,is_available,price,id} = data
     return(
         <div className={style.Output}>
             <div className={style.lef}>
@@ -17,9 +17,9 @@ const Output = ({data,increaseCount,decreaseCount})=>{
             </div>
             <div className={style.lef}>
                 <div id={style.cnt}>
-                    <AiOutlineMinus className={style.inc} onClick={()=>decreaseCount(count)}></AiOutlineMinus>
-                    <p id={style.val}>{count}</p>
-                    <AiOutlinePlus className={style.inc} onClick={()=>increaseCount(count)}></AiOutlinePlus>
+                    <AiOutlineMinus className={style.inc} onClick={()=>setCounteer(counter>0?counter-1:counter)}></AiOutlineMinus>
+                    <p id={style.val}>{counter}</p>
+                    <AiOutlinePlus className={style.inc} onClick={()=>setCounteer(counter>=0?counter+1:counter)}></AiOutlinePlus>
                 </div>
                 <p id={style.stock} style={{backgroundColor:is_available?"rgb(213,232,212)":"rgb(248,206,204)",border:is_available?"1px solid green":"1px solid red"}}>{is_available?"High Stock":"Low Stock"}</p>
             </div>
